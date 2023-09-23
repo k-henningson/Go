a10b61dd8ec5c444db854506903b0e80ac72ee7b
Kyra Henningson <kyra.henningson@dapperlabs.com>
2023-09-23 16:23:02 -0700

    add FAQ page

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -15,20 +15,18 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:123@gmail.com\">email@email.com</a>.")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text-html: charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+	<li><b>Question 1:</b></li>
+	<li><b>Question 2:</b></li>
+	<li><b>Question 3:</b></li>
+	</ul>
+	`)
 }
 
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
 // 		homeHandler(w, r)
@@ -39,7 +37,23 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	}
 // }
 
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func main() {
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", router)
 }
