d682e06544d9157a620cf7989b20e0468397c840
Kyra Henningson <kyrahenningson@hotmail.com>
2024-01-15 11:36:45 -0800

    fmt.Errorf lesson

--- lenslocked/cmd/exp/main.go
+++ lenslocked/cmd/exp/main.go
@@ -1,30 +1,55 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"errors"
+	"fmt"
 )
 
-type User struct {
-	Name string
-	Bio  string
-	Age  int
-}
+// type User struct {
+// 	Name string
+// 	Bio  string
+// 	Age  int
+// }
+
+// func main() {
+// 	t, err := template.ParseFiles("hello.gohtml")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	user := User{
+// 		Name: "John Smith",
+// 		Bio:  `<script>alert("Cross Site Scripting lesson");</script>`,
+// 		Age:  123,
+// 	}
+
+// 	err = t.Execute(os.Stdout, user)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
+	err := CreateOrg()
+	fmt.Println((err))
+}
 
-	user := User{
-		Name: "John Smith",
-		Bio:  `<script>alert("Cross Site Scripting lesson");</script>`,
-		Age:  123,
-	}
+func Connect() error {
+	return errors.New("connection failed")
+}
 
-	err = t.Execute(os.Stdout, user)
+func CreateUser() error {
+	err := Connect()
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("create user: %w", err)
 	}
+	return nil
+}
+
+func CreateOrg() error {
+	err := CreateUser()
+	if err != nil {
+		return fmt.Errorf("create org: %w", err)
+	}
+	return nil
 }
